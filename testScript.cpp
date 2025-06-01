78c3cb96f1b7f68448edc11daf548e5d99c730b6
Mr Plenty <allan_leeman20@hotmail.co.uk>
2025-06-01 14:15:21 +0100

    OnClick event handler written, damn I'm good

--- testScript.cpp
+++ testScript.cpp
@@ -16,12 +16,33 @@
 #include "DialogueSystem.h"
 
 bool running = true;
+bool waitingForAdvance = true;
+
 SDL_Event event;
 
 
 
 const int FPS = 60;
 const int frameDelay = 1000 / FPS;
+
+void HandleEventsAndAdvance(SDL_Renderer* renderer, bool& waitingForAdvance) {
+	
+	while (SDL_PollEvent(&event)) {
+		if (event.type == SDL_QUIT) {
+			running = false;
+		}
+		if (event.type == SDL_MOUSEBUTTONDOWN && waitingForAdvance) {
+			if (event.button.button == SDL_BUTTON_LEFT) {
+				dialogue->Advance(renderer);
+				waitingForAdvance = false;
+			}
+
+		}
+		if (event.type == SDL_MOUSEBUTTONUP) {
+			waitingForAdvance = true;
+		}
+	}
+}
 
 void ch_0(SDL_Renderer* renderer, TTF_Font* font){
 
@@ -30,12 +51,9 @@ void ch_0(SDL_Renderer* renderer, TTF_Font* font){
 	while (running) {
 		Uint32 frameStart = SDL_GetTicks();
 
+		HandleEventsAndAdvance(renderer, waitingForAdvance);
 
-		while (SDL_PollEvent(&event)) {
-			if (event.type == SDL_QUIT) {
-				running = false;
-			}
-		}
+		
 
 		SDL_RenderClear(renderer); // clear the screen
 		
@@ -43,7 +61,7 @@ void ch_0(SDL_Renderer* renderer, TTF_Font* font){
 		//LoadBackground(renderer, backgroundBatch, 30); // Load the background image
 		
 
-		dialogue->Advance(renderer);
+		//dialogue->Advance(renderer);
 		dialogue->Render(renderer, &textRect);
 
 		
